83e46b6b3d4a1854a0803a9b47d23dc071c755bb
LadeedAhamed <ahamedladeed@gmail.com>
2024-07-29 09:38:57 +0530

    UI Login: signup screen success screen verify email screen

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,6 +1,8 @@
+import 'package:courtsides/features/authentication/screens/signup/signup.dart';
 import 'package:courtsides/utils/constants/sizes.dart';
 import 'package:courtsides/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class TLoginForm extends StatelessWidget {
@@ -77,7 +79,7 @@ class TLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(() => const SignUpScreen()),
                 child: const Text(TTexts.createAccount),
               ),
             ),
