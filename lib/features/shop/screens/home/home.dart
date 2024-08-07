89f3207b362845c4875e1bba8bcbb3baef8104ed
LadeedAhamed <ahamedladeed@gmail.com>
2024-08-07 09:18:58 +0530

    Home Screen:  custom appbar complete

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,9 @@
+import 'package:courtsides/common/widgets/appbar/appbar.dart';
+import 'package:courtsides/common/widgets/products_cart/cart_menu_icon.dart';
+import 'package:courtsides/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:courtsides/features/shop/screens/home/widgets/primary_header_container.dart';
+import 'package:courtsides/utils/constants/colors.dart';
+import 'package:courtsides/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,10 +11,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [TPrimaryHeaderContainer(child: Container())],
+          children: [
+            TPrimaryHeaderContainer(
+                child: Column(
+              children: [THomeAppBar()],
+            ))
+          ],
         ),
       ),
     );
